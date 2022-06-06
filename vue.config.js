264cf4b8be923cf0658f6a49cc121b71168d9b21
Jacob <jacob@Jacob-2.local>
2022-06-07 07:40:01 +0800

    commit

--- vue.config.js
+++ vue.config.js
@@ -3,9 +3,9 @@ console.log(name,'name;;')
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath:process.env.NODE_ENV==='production'?'`/app1/`':'./',
+  publicPath:process.env.NODE_ENV==='production'?'/app1/':'./',
   devServer:{
-    port:8081,
+    port:9528,
     headers: {
       'Access-Control-Allow-Origin': '*' // 主应用获取子应用时跨域响应头
     }
